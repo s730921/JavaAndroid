211fca30911cce7bb6dea7e6a418569bafa251c2
s730921 <shson@assesta.com>
2017-12-04 21:51:17 +0900

    2017-12-04

--- workspace/04. 주민번호 체크/src/Jumincheck.java
+++ workspace/04. 주민번호 체크/src/Jumincheck.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Jumincheck {
@@ -25,6 +27,33 @@ public class Jumincheck {
 		
 		if (result == JuminNo.charAt(13) - 48) {
 			System.out.println("주민번호 정상임!!");
+			
+			//나이 추출
+			Calendar cal = Calendar.getInstance(Locale.KOREA);
+			int year = cal.get(Calendar.YEAR);	//현재 년도
+			int yy = Integer.parseInt(JuminNo.substring(0, 2));	//"73" -> 73
+			
+			if((JuminNo.charAt(7) - 48) < 3) {
+				yy = yy + 1900;
+			} else {
+				yy = yy + 2000;
+			}
+			
+			int Age = year - yy + 1;	//본나이
+			
+			System.out.println("나이 : " + Age);
+			
+			//성별 추출
+			//int Sex = (JuminNo.charAt(7) - 48);
+			if ((JuminNo.charAt(7) - 48) % 2 == 0) {	//2,4면 여자
+				System.out.println("성별 : 여자");
+			} else {
+				System.out.println("성별 : 남자");
+			}
+			
+			//생년월일 추출 - 73/09/21
+			System.out.println("생년월일 : " + yy + "/" + JuminNo.substring(2, 4) + "/" + JuminNo.substring(4, 6));
+						
 		} else {
 			System.out.println("주민번호 틀림!!");
 		}
